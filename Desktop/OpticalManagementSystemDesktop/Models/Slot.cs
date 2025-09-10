d9aa96358ed31e926b89392ab58bbb5573aa6b5d
jadeprideaux <jadeprideaux7@gmail.com>
2025-09-10 11:34:41 +0100

    Added slot model class on frontend to represent an appointment slot when displaying the optom calendars.

---
+++ Desktop/OpticalManagementSystemDesktop/Models/Slot.cs
@@ -0,0 +1,10 @@
+﻿namespace OpticalManagementSystemDesktop.Models
+{
+    // Class to represent a slot on the frontend
+    public class Slot
+    {
+        public DateTime StartTime { get; set; }
+        public Appointment? Appointment { get; set; }
+        public bool IsBooked => Appointment != null;
+    }
+}
